d1ae8bc968d95c8e5d08ac4ce7577df9df6ff867
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-23 17:26:25 +0530

    Add models for movie app
    Add necessary model and model fields in movie app

--- movieapp/models.py
+++ movieapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    prefix = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    director = models.ManyToManyField('Director')
+    studio = models.ForeignKey('Studio',on_delete=models.CASCADE)
+    released_date = models.DateField()
+    cover_image = models.ImageField(upload_to='movieapp/static/images',null=True,blank=True)
+    review = models.TextField(max_length=500)
+    genre = models.ManyToManyField('Genre')
+    asin = models.UUIDField(unique=True, default=uuid.uuid4)
+
+class Studio(models.Model):
+    title = models.CharField(max_length=200)
+    prefix = models.CharField(max_length=200)
+    website = models.URLField(null=True, blank=True)
+    slug = models.SlugField(max_length=200)
+
+class Genre(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=12)
+    website = models.URLField(null=True, blank=True)
+    birthday = models.DateField()
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER,
+        blank=True,
+        help_text='gender',
+    )
