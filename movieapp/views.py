df912694d3cd99d268c121385b08011dc9b73809
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-24 18:14:20 +0530

    Add index view for movie app
    Add home page for movie app

--- movieapp/views.py
+++ movieapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Movie, Director, Studio, Genre
 
-# Create your views here.
+def show_index(request):
+    context = {
+        'total_movie': Movie.objects.all().count(),
+        'total_directors': Director.objects.all().count(),
+        'total_studio': Studio.objects.all().count(),
+        'total_genre': Genre.objects.all().count(),
+    }
+    return render(request, 'movieapp/index.html', context)
